30832e3b6862a4689280e916dd091bd7c39160ec
HU <35089763+bd15155@users.noreply.github.com>
2020-03-21 12:59:50 +0900

    eyecontact

--- Assets/eyeball2.cs
+++ Assets/eyeball2.cs
@@ -1,18 +1,78 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Tobii.Gaming;
+
+namespace TobiiEyeTracking
+{
+    public enum UpdateType
+{
+    None,
+    Update,
+    LateUpdate,
+}
 
 public class eyeball2 : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public UpdateType UpdateType = UpdateType.Update;
+    public GameObject LookTarget;
+    private Vector3 StartPos;
+    public float Renge_X;
+    public float Renge_Y;
+    public bool Invert_X;
+    public bool Invert_Y;
+    public float FilterSmoothingFactor = 0.15f;
+    private bool _hasHistoricPoint;
+    private Vector3 _historicPoint;
+
     void Start()
     {
-        
+        StartPos = LookTarget.transform.position;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (UpdateType == UpdateType.Update)
+        {
+            GazePoint gazePoint = TobiiAPI.GetGazePoint();
+            Renge_X = Mathf.Abs(Renge_X) * (Invert_X ? 1 : -1);
+            Renge_Y = Mathf.Abs(Renge_Y) * (Invert_Y ? 1 : -1);
+
+            Vector3 gazePointInWorld = new Vector3(StartPos.x + ((gazePoint.Viewport.x - 0.5f) * Renge_X), StartPos.y + ((gazePoint.Viewport.y - 0.5f) * Renge_Y), StartPos.z);
+            LookTarget.transform.position = Smoothify(gazePointInWorld);
+        }
     }
+
+    void LateUpdate()
+    {
+        if (UpdateType == UpdateType.LateUpdate)
+        {
+            GazePoint gazePoint = TobiiAPI.GetGazePoint();
+            Renge_X = Mathf.Abs(Renge_X) * (Invert_X ? 1 : -1);
+            Renge_Y = Mathf.Abs(Renge_Y) * (Invert_Y ? 1 : -1);
+
+            Vector3 gazePointInWorld = new Vector3(StartPos.x + ((gazePoint.Viewport.x - 0.5f) * Renge_X), StartPos.y + ((gazePoint.Viewport.y - 0.5f) * Renge_Y), StartPos.z);
+            LookTarget.transform.position = Smoothify(gazePointInWorld);
+        }
+    }
+
+    private Vector3 Smoothify(Vector3 point)
+    {
+
+        if (!_hasHistoricPoint)
+        {
+            _historicPoint = point;
+            _hasHistoricPoint = true;
+        }
+
+        var smoothedPoint = new Vector3(
+            point.x * (1.0f - FilterSmoothingFactor) + _historicPoint.x * FilterSmoothingFactor,
+            point.y * (1.0f - FilterSmoothingFactor) + _historicPoint.y * FilterSmoothingFactor,
+            point.z);
+
+        _historicPoint = smoothedPoint;
+
+        return smoothedPoint;
+    }
+}
 }
