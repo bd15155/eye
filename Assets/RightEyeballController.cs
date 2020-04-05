7a1ab9fe3dcda3dfc6e089fb7f338eab710b8d16
Tomohiko Mukai <contact@mukai-lab.org>
2020-04-05 09:36:18 +0900

    修正提案

---
+++ Assets/RightEyeballController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RightEyeballController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 出力: transform.localRotation
+        // 入力1: transform.position;
+        // 入力2: TobiiAPI.GetGazePoint
+        // 方法: Camera.ScreenToWorldPoint
+        //        ↑ おそらく
+    }
+}
